421121f8212da81c115a186121cc16a1ab1d5d05
vernonroque <vroque88@gmail.com>
2024-02-19 16:57:23 -0500

    Added initial files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MainCard from './components/MainCard';
+
+function App() {
+  return (
+    <main>
+      <MainCard/>
+    </main>
+   
+  );
+}
+
+export default App;
---
+++ src/components/MainCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './mainCard.module.css'
+
+function MainCard() {
+  return (
+    <main>MainCard</main>
+  )
+}
+
+export default MainCard
