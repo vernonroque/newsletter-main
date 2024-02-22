4abe803d0f1156aa32a12b1e3782ef42b19b20d5
vernonroque <vroque88@gmail.com>
2024-02-22 12:55:05 -0500

    Cleaned up stuff

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -1,7 +1,6 @@
-import React,{useState,useEffect} from 'react'
+import React,{useState} from 'react'
 import styles from './mainCard.module.css'
 import signUpImg from '../assets/images/illustration-sign-up-desktop.svg'
-import signUpImgMobile from '../assets/images/illustration-sign-up-mobile.svg'
 import iconList from '../assets/images/icon-list.svg'
 
 function MainCard() {
@@ -35,22 +34,7 @@ function MainCard() {
         setEmail(e.target.value);
         setError(false);
     }
-    // function handleResize(){
-    //     setWidth(window.innerWidth);
-
-    // }
-    // useEffect(()=>{
-    //     window.addEventListener('resize',handleResize)
-    //     if(width <=380){
-    //         //setMobile(true);
-    //         console.log("Mobile mode")
-    //     }
-
-    //     return ()=>{
-    //         window.addEventListener('resize',handleResize)
-    //     }
-
-    // },[width])
+   
   return (
     <main className = {!success ? styles.mainStuff:styles.mainStuffSuccess}>
         {!success ?
