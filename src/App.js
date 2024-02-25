2769d4b4cec0a2b5d01d3ab5fb9ca8c250504f4f
vernonroque <vroque88@gmail.com>
2024-02-25 18:20:34 -0500

    Added attribution

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ function App() {
   return (
     <main className = "App">
       <MainCard/>
+      <div className="attribution">
+
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel = "noreferrer">Frontend Mentor</a>. 
+        Coded by <a href="#">Mr. Rogue</a>.
+
+      </div>
     </main>
    
   );
